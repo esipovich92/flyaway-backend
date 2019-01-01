60f8e6461c7c13a0ca8dc071a20941aa65423f87
artem <esipovich92@gmail.com>
2019-01-01 20:09:20 +0300

    FlyAway-1018 Created GlobalExceptionHandler

---
+++ src/main/java/com/esipovich/flyaway/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.esipovich.flyaway;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @author Artem Esipovich 1/1/19
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
